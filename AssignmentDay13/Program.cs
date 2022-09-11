28e0f63acc3a828df9283133de577bc2a232d53e
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-11 11:03:53 +0530

    [roshan]maximum number using three number

---
+++ AssignmentDay13/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AssignmentDay13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welecome to generic method");
+            Console.WriteLine("enter chooes\n1.maximum number");
+            int opt=Convert.ToInt32(Console.ReadLine());
+            
+            switch(opt)
+            {
+                case 1:
+                    MaxNumber maxNumber = new MaxNumber();
+                    int val = maxNumber.PrintMaxNumber<int>(23, 45, 60);
+                    Console.WriteLine("max number is "+val);
+                    break;
+            }
+
+            
+        }
+    }
+}
